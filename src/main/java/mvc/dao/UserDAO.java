8b5161fffabba9fd02c97678ceb14c55cfb511a0
radioheadxx <holdenkolfild@inbox.ru>
2022-06-24 17:58:12 +0300

    Initial commit

---
+++ src/main/java/mvc/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package mvc.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDAO {
+
+}
---
+++ src/main/java/mvc/model/User.java
@@ -0,0 +1,52 @@
+package mvc.model;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String surname;
+
+    public User() {
+    }
+
+    public User(long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
